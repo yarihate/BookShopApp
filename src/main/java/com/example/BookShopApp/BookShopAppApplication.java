2ca79f84712d130606cac4b9e6c4844f463b08ae
Valeriya Gelasimova <valeriyagelasimova@gmail.com>
2021-05-02 17:15:50 +0300

    Initial commit

---
+++ src/main/java/com/example/BookShopApp/BookShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookShopApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookShopApp/BookShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookShopApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

346b134697174445b456ee671aa71e5685d4ecc5
Valeriya Gelasimova <valeriyagelasimova@gmail.com>
2021-05-22 22:01:24 +0300

    4.2 Thymeleaf - Template Engine

--- src/main/java/com/example/BookShopApp/controllers/MainPageController.java
+++ src/main/java/com/example/BookShopApp/controllers/MainPageController.java
@@ -21,6 +21,7 @@ public class MainPageController {
     @GetMapping("/main")
     public String mainPage(Model model) {
         model.addAttribute("bookData", bookService.getBooksData());
+        model.addAttribute("searchPlaceholder", "new search placeholder");
         return "index";
     }
 }

5eb9e80327086da81b2850359ffca863fae57c14
Valeriya Gelasimova <valeriyagelasimova@gmail.com>
2021-06-24 21:49:55 +0300

    5.7 Домашняя работа Задание 1. Переписывание AuthorService с использованием JPA

--- src/main/java/com/example/BookShopApp/data/AuthorService.java
+++ src/main/java/com/example/BookShopApp/data/AuthorService.java
@@ -12,22 +12,15 @@ import java.util.stream.Collectors;
 @Service
 public class AuthorService {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final AuthorRepository authorRepository;
 
     @Autowired
-    public AuthorService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
     }
 
     public Map<String, List<Author>> getAuthorsData() {
-        List<Author> authors = jdbcTemplate.query("SELECT * FROM authors", (ResultSet rs, int row) -> {
-            Author author = new Author();
-            author.setId(rs.getInt("id"));
-            author.setFirstName(rs.getString("first_name"));
-            author.setLastName(rs.getString("last_name"));
-            return author;
-        });
-        //  authors.sort(Comparator.comparing(Author::getFullName));
+        List<Author> authors = authorRepository.findAll();
         return authors.stream().collect
                 (Collectors.groupingBy(author -> String.valueOf(author.getLastName().charAt(0))));
     }
